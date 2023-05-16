f131c0359a0f5660787469d5bb089112da535c9c
AungKo2019 <highflier.ako1991@gmail.com>
2023-05-16 11:53:40 +0630

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import * as React from 'react';
+import * as Clipboard from 'expo-clipboard';
 
 export default function App() {
+
+const [copiedtext,setcopiedtext]=React.useState('');
+
+const  copyToClipboard=()=>{
+  Clipboard.setString(text);
+}
+
+const text="hello developers"
+
+const fetchCopiedText=async()=>{
+  const text=await Clipboard.getStringAsync();
+  setcopiedtext(text);
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{margin:20}}>
+      <Text selectable>{text}</Text>
+      <Button title="copy Button" onPress={copyToClipboard}/>
+      <Button title="view copied Text" onPress={fetchCopiedText}/>
+      <Text style={styles.copiedText}>{copiedtext}</Text>
+    </View>
     </View>
   );
 }
@@ -17,4 +37,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  copiedText:{
+    margin:10,
+    color:'red'
+  }
 });
